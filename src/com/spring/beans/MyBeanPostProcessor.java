9eb48037adc5d4d0d4167dfb6e770c7f3c2add5d
591130877 <591130877@qq.com>
2017-06-02 12:43:28 +0800

    Initial commit

---
+++ src/com/spring/beans/MyBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package com.spring.beans;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("正在进行初始化之后的处理，处理" + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("正在进行初始化之前的处理，处理" + beanName);
+		return bean;
+	}
+
+}
---
+++ src/com/spring/beans/Person.java
@@ -0,0 +1,41 @@
+package com.spring.beans;
+
+public class Person {
+	private String name;
+	private int age;
+	private Car car;
+	public Person() {
+		System.out.println("构造器");
+	}
+	public void a() {
+		System.out.println("初始化方法");
+	}
+	public void b() {
+		System.out.println("销毁对象...");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		System.out.println("setName方法");
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		System.out.println("setAge方法");
+		this.age = age;
+	}
+	public Car getCar() {
+		return car;
+	}
+	public void setCar(Car car) {
+		System.out.println("setCar方法");
+		this.car = car;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", car=" + car + "]";
+	}
+}
